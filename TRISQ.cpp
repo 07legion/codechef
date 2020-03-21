6b61ea0e84404c3276fcd768e26d63d27cc10eb8
07legion <36340067+07legion@users.noreply.github.com>
2020-03-22 01:17:21 +0530

    Add files via upload

---
+++ TRISQ.cpp
@@ -0,0 +1,52 @@
+                                     /* WORK HARD */
+#include <bits/stdc++.h> 
+#include<assert.h>
+typedef long long ll; 
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forn(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define for1(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define ford(i, n) for (ll i = (ll)(n) - 1; i >= 0; i--)
+//#define fori(i, a, b) for (ll i = (ll)(a); i <= (ll)(b); ++i)
+#define fori(i,a,b) for(ll i=a;i<b;i++)
+#define fore(i,a,b) for(ll i=a;i<=b;i++)
+#define debug1(x) cerr << "at line " << __LINE__ << ": " << #x << " = " << (x) << endl
+#define debug2(x) cerr << #x << " = " << (x) << endl
+#define MAX 100000
+#define endl "\n"
+using namespace std;
+template<typename Args>
+void kill(Args args){cout << args << "\n";exit(0);}
+typedef double ld;
+typedef string st;
+const ll MOD=1e9 + 7 ;
+const ll INF=1e14;                                                                                           
+ll mpow(ll a,ll b,ll p=MOD){a=a%p;ll res=1;while(b>0){if(b&1)res=(res*a)%p;a=(a*a)%p;b=b>>1;}return res%p;}             
+void precompute(){}
+ll recurse(ll B){
+    if(B==1 || B==2 || B==3) return 0;
+    return ((B/2-1)+recurse(B-2));
+}
+int main(){ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin); 
+    freopen("output.txt","w",stdout);
+    #endif
+    clock_t zzzx=clock();
+    ll T;cin>>T;
+    while(T--){
+        ll B;cin>>B;
+        ll ans=recurse(B);
+        cout<<ans<<"\n";
+    }
+    double elapsed_time=(double)(clock()-zzzx)/CLOCKS_PER_SEC;
+    debug2(elapsed_time);
+    return 0;
+} 
